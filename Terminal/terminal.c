59e2c165b139a99f6ba862ac703f795fdf3dddbe
AbdelrahmanKandil <3bdulra7man.kandil@gmail.com>
2022-10-26 16:22:10 +0200

    Terminal Files

---
+++ Terminal/terminal.c
@@ -0,0 +1,148 @@
+/********************************************************************************************/
+/********************************************************************************************/
+/**************************		Author:  Abdelrahman Kandil			            *************/
+/**************************		Date:    10 October 2022	            		*************/
+/**************************		Layer:   App								   	*************/
+/**************************		SWC:	 terminal.c								*************/
+/**************************		Version: 1.00 	             					*************/
+/********************************************************************************************/
+/********************************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS 1 
+#define _WINSOCK_DEPRECATED_NO_WARNINGS 1 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<string.h>
+#include"terminal.h"
+
+#include "../STD_TYPES.h"
+EN_terminalError_t getTransactionDate(ST_terminalData_t* termData)
+{
+	EN_terminalError_t err;
+	uint8_t transactionDate[11];
+	time_t currentTimeInSec = time(NULL_PTR);
+	struct tm* currentLocalTimeStruct = localtime(&currentTimeInSec);
+	uint8_t currentDay = currentLocalTimeStruct->tm_mday, currentMon = currentLocalTimeStruct->tm_mon+1;
+	uint32_t currentYear = currentLocalTimeStruct->tm_year + 1900;
+
+	if(currentDay > 9)
+	{
+		transactionDate[0] = (currentDay / 10)+'0';
+		transactionDate[1] = (currentDay % 10) +'0';
+	}
+	else
+	{
+		transactionDate[0] = '0';
+		transactionDate[1] = currentDay +'0';
+	}
+	transactionDate[2] = '/';
+
+
+	if (currentMon > 9)
+	{
+		transactionDate[3] = (currentMon / 10) + '0';
+		transactionDate[4] = (currentMon % 10) + '0';
+	}
+	else
+	{
+		transactionDate[3] = '0';
+		transactionDate[4] = currentMon + '0';
+	}
+	transactionDate[5] = '/';
+
+
+	for (int i = 3; i >=0; i--)
+	{
+		transactionDate[6 + i] = (currentYear % 10)+'0';
+		currentYear /= 10;
+	}
+	transactionDate[10] = '\0';
+
+	memcpy(termData->transactionDate, transactionDate, 11);
+	err = TERMINAL_OK;
+	return err;
+}
+
+EN_terminalError_t isCardExpired(ST_cardData_t* cardData, ST_terminalData_t* termData)
+{
+	// 08/22
+	// 12/08/2022
+
+	uint8_t cardExpMon[3] = { cardData->cardExpirationDate[0],cardData->cardExpirationDate[1], '\0' };
+	uint8_t cardExpYear[3] = { cardData->cardExpirationDate[3],cardData->cardExpirationDate[4], '\0'};
+
+
+	uint8_t TransacMon[3] = { termData->transactionDate[3],termData->transactionDate[4], '\0' };
+	uint8_t TransacYear[3] = { termData->transactionDate[8],termData->transactionDate[9], '\0'};
+
+	
+	if (atoi(cardExpYear) > atoi(TransacYear)) return TERMINAL_OK;
+	else if (atoi(cardExpYear) < atoi(TransacYear)) return EXPIRED_CARD;
+	else
+	{
+		if (atoi(cardExpMon) >= atoi(TransacMon)) return TERMINAL_OK;
+		else return EXPIRED_CARD;
+	}
+
+	 
+
+}
+
+EN_terminalError_t isValidCardPAN(ST_cardData_t* cardData)
+{
+	char i;
+	unsigned char multNum, luhnTot = 0,oddElementFlag = 0;
+	unsigned char panLen = strlen(cardData->primaryAccountNumber);
+
+	for (i = panLen-1; i>=0; i--)
+	{
+
+		if (oddElementFlag)
+		{
+			multNum = 2 * (cardData->primaryAccountNumber[i] - '0');
+			if (multNum > 9) multNum = (multNum % 10) + (multNum / 10);
+		}
+		else
+		{
+			multNum = cardData->primaryAccountNumber[i] - '0';
+		}
+
+		oddElementFlag = !oddElementFlag;
+		luhnTot += multNum;
+
+	}
+	return luhnTot % 10 == 0 ? CARD_OK: INVALID_CARD;
+}
+EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData)
+{
+	float transactionAmount;
+
+	printf("Please enter the transaction amount: ");
+	scanf("%f", &transactionAmount);
+	getchar();
+	if (transactionAmount <= 0) return INVALID_AMOUNT;
+	
+	termData->transAmount = transactionAmount;
+	return TERMINAL_OK;
+
+}
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData)
+{
+	
+	return termData->transAmount > termData->maxTransAmount ? EXCEED_MAX_AMOUNT : TERMINAL_OK;
+}
+EN_terminalError_t setMaxAmount(ST_terminalData_t* termData)
+{
+	float maxTransactionAmount;
+
+	printf("Please enter the maximum transaction amount: ");
+	scanf("%f", &maxTransactionAmount);
+	getchar();
+	if (maxTransactionAmount <= 0) return INVALID_MAX_AMOUNT;
+
+	termData->maxTransAmount = maxTransactionAmount;
+	return TERMINAL_OK;
+
+}
+
